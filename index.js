64b12b41e5e2ed7c8560cf58a35e947a4ee2d6c4
Elijah Wines <wines.elijah@gmail.com>
2021-02-11 15:26:31 -0500

    wrote seudo code for js

---
+++ index.js
@@ -0,0 +1,20 @@
+// When screen loads, user is prompted with game board
+
+// user has one option - START GAME
+    // if user starts game 
+        // start game text changes to reset game
+        // timer appears and starts for one minute
+            // if timer hits 0
+                // users score is sent to the score in game over div
+                // game score is shown
+        // fruit begins to fall
+            // fruit falls from top to bottom of screen
+                // if user slices fruit
+                    // score increase by one
+                // if user missed fruit
+                    // score decrease by one
+
+    // if user ends game
+        // score is set to zero
+        // game over div display is none
+        // time is no longer showing and is set to 60 seconds
