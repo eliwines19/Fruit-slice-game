f74e3107b3fb519cf1d7b39e6c9901e777829fe4
Elijah Wines <wines.elijah@gmail.com>
2021-02-15 16:54:01 -0500

    configured for jquery, btn html changes on click, created playingStatus var

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 // When screen loads, user is prompted with game board
+window.addEventListener('load', function(){
 
-// user has one option - START GAME
+    $("#start-or-reset").click(startGame)
+
+    var playingStatus = false
+
+    function changeText(btn) {
+        if (btn.html() === "Release the fruit!") {
+            playingStatus = true;
+            $("#start-or-reset").html("Stop the Fruit!")
+            $("#start-or-reset").css({
+                "background-color" : "red",
+                "color" : "white"
+            })
+        } else {
+            playingStatus = false;
+            $("#start-or-reset").html("Release the fruit!")
+            $("#start-or-reset").css({
+                "background-color" : "#966F33",
+                "color" : "black"
+            })
+        }
+    }
+
     // if user starts game 
+    function startGame() {
         // start game text changes to reset game
+        changeText($("#start-or-reset"))
         // timer appears and starts for one minute
             // if timer hits 0
                 // users score is sent to the score in game over div
@@ -13,6 +37,9 @@
                     // score increase by one
                 // if user missed fruit
                     // score decrease by one
+    }
+
+})
 
     // if user ends game
         // score is set to zero
