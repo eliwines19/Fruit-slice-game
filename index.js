963a8ae1b6a3063f4e0f92389894f768fc501c6f
Elijah Wines <wines.elijah@gmail.com>
2021-02-24 22:24:45 -0500

    mouse trail and fruit click removes fruit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-window.addEventListener('load', function(){
-
+$(function(){
     // contingent on text inside the #start-or-reset divs inner html
     var playingStatus = false
 
@@ -66,7 +65,7 @@ window.addEventListener('load', function(){
         const score = $("#score-value").html();
         $("#final-score").html(score)
     }
-    
+
     function showGameOver() {
         $("#timer").hide()
         $("#game-over").show()
@@ -91,20 +90,14 @@ window.addEventListener('load', function(){
             "margin-left": `${randomInteger(5, 80)}%`
         })
         $("#fruit-board").append(fruitDiv)
-        addClickEvent(fruitDiv)
+        createClickEvent(fruitDiv)
         makeFruitFall(fruitDiv)
     }
 
-    function addClickEvent(fruit) {
+    function createClickEvent(fruit){
         fruit.click(function(){
-            // remove fruit
             fruit.remove()
-            // add 1 point
-            let scoreValue = parseInt($("#score-value").html())
-            scoreValue += 1;
-            $("#score-value").html(scoreValue.toString())
-
-            // make popup for hit fruit (boom, bam, booyah)
+            // add one to score
         })
     }
 
@@ -122,7 +115,71 @@ window.addEventListener('load', function(){
         }, 10)
     }
 
+    // dots is an array of Dot objects,
+    // mouse is an object used to track the X and Y position
+    // of the mouse, set with a mousemove event listener below
+    var dots = [], mouse = { x: 0, y: 0};
 
+    // The Dot object used to scaffold the dots
+    var Dot = function() {
+        this.x = 0;
+        this.y = 0;
+        this.node = (function(){
+            var n = document.createElement("div");
+            n.className = "trail";
+            document.body.appendChild(n);
+            return n;
+        }());
+    };
+
+    // The Dot.prototype.draw() method sets the position of 
+    // the object's <div> node
+    Dot.prototype.draw = function() {
+        this.node.style.left = `${this.x + 3}px`;
+        this.node.style.top = `${this.y + 3}px`;
+    };
+
+    // Creates the Dot objects, populates the dots array
+    for (var i = 0; i < 15; i++) {
+        var d = new Dot();
+        dots.push(d);
+    }
+
+    // This is the screen redraw function
+    function draw() {
+        // Make sure the mouse position is set everytime
+        // draw() is called.
+        var x = mouse.x, y = mouse.y;
+
+        // This loop is where all the 90s magic happens
+        dots.forEach(function(dot, index, dots) {
+            var nextDot = dots[index + 1] || dots[0];
+
+            dot.x = x;
+            dot.y = y;
+            dot.draw();
+            x += (nextDot.x - dot.x) * .5;
+            y += (nextDot.y - dot.y) * .5;
+
+        });
+
+    }
+
+    addEventListener("mousemove", function(event) {
+        //event.preventDefault();
+        mouse.x = event.pageX;
+        mouse.y = event.pageY;
+    });
+
+    // animate() calls draw() then recursively calls itself
+    // everytime the screen repaints via requestAnimationFrame().
+    function animate() {
+        draw();
+        requestAnimationFrame(animate);
+    }
+
+    // And get it started by calling animate().
+    animate();
 
 })
 
