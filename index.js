6f9bf22ad3d12fd2d4745c2efd4870285498bf1c
Elijah Wines <wines.elijah@gmail.com>
2021-02-26 09:01:42 -0500

    changed file name, added sword, fruit mouseover effect

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ $(function(){
     }
 
     function fallingFruit() {
-        let interval = setInterval(chooseFruit, randomInteger(2000, 6000))
+        let interval = setInterval(chooseFruit, randomInteger(1000, 3000))
         if (playingStatus === false) {
             clearInterval(interval)
         }
@@ -86,19 +86,12 @@ $(function(){
         const fruitDiv = $("<div></div>");
         fruitDiv.addClass("fruit");
         fruitDiv.css({
-            "background-image": `url("fruit/${randomInteger(1, 30)}.png")`,
+            "background-image": `url("icons/${randomInteger(1, 30)}.png")`,
             "margin-left": `${randomInteger(5, 80)}%`
         })
         $("#fruit-board").append(fruitDiv)
-        createClickEvent(fruitDiv)
         makeFruitFall(fruitDiv)
-    }
-
-    function createClickEvent(fruit){
-        fruit.click(function(){
-            fruit.remove()
-            // add one to score
-        })
+        applyMouseOver(fruitDiv)
     }
 
     function makeFruitFall(fruit) {
@@ -115,71 +108,18 @@ $(function(){
         }, 10)
     }
 
-    // dots is an array of Dot objects,
-    // mouse is an object used to track the X and Y position
-    // of the mouse, set with a mousemove event listener below
-    var dots = [], mouse = { x: 0, y: 0};
-
-    // The Dot object used to scaffold the dots
-    var Dot = function() {
-        this.x = 0;
-        this.y = 0;
-        this.node = (function(){
-            var n = document.createElement("div");
-            n.className = "trail";
-            document.body.appendChild(n);
-            return n;
-        }());
-    };
-
-    // The Dot.prototype.draw() method sets the position of 
-    // the object's <div> node
-    Dot.prototype.draw = function() {
-        this.node.style.left = `${this.x + 3}px`;
-        this.node.style.top = `${this.y + 3}px`;
-    };
-
-    // Creates the Dot objects, populates the dots array
-    for (var i = 0; i < 15; i++) {
-        var d = new Dot();
-        dots.push(d);
+    function applyMouseOver(fruit) {
+        fruit.mouseover(function(){
+            fruit.remove()
+            addPoint()
+        })
     }
 
-    // This is the screen redraw function
-    function draw() {
-        // Make sure the mouse position is set everytime
-        // draw() is called.
-        var x = mouse.x, y = mouse.y;
-
-        // This loop is where all the 90s magic happens
-        dots.forEach(function(dot, index, dots) {
-            var nextDot = dots[index + 1] || dots[0];
-
-            dot.x = x;
-            dot.y = y;
-            dot.draw();
-            x += (nextDot.x - dot.x) * .5;
-            y += (nextDot.y - dot.y) * .5;
-
-        });
-
+    function addPoint(){
+        let score = parseInt($("#score-value").html())
+        score += 1;
+        $("#score-value").html(score.toString())
     }
-
-    addEventListener("mousemove", function(event) {
-        //event.preventDefault();
-        mouse.x = event.pageX;
-        mouse.y = event.pageY;
-    });
-
-    // animate() calls draw() then recursively calls itself
-    // everytime the screen repaints via requestAnimationFrame().
-    function animate() {
-        draw();
-        requestAnimationFrame(animate);
-    }
-
-    // And get it started by calling animate().
-    animate();
 
 })
 
